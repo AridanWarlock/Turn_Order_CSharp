9a9b0ea546cdeb8481073116465b6da75aeb7fe0
AridanWarlock <Krasawa34rus@yandex.ru>
2024-01-13 22:21:37 +0300

    Мелкие правки

--- Fighter.cs
+++ Fighter.cs
@@ -2,11 +2,20 @@
 {
     internal abstract class Fighter : IComparable 
     {
-        public string? actor;
-        public readonly string Name;
+        private readonly string _name;
         private int _initiative;
         private int _health;
         private int _max_health;
+        public Fighter(string name, int initiative, int health, int max_health)
+        {
+            if (!name.All(char.IsLetterOrDigit))
+                throw new Fighter_Exception("Имя должно состоять только из букв и цифр!");
+            _name = name;
+            Initiative = initiative;
+            Health = health;
+            Max_health = max_health;
+        }
+
         public int Initiative
         {
             get => _initiative;
@@ -28,23 +37,17 @@
                 ? value
                 : throw new Fighter_Exception("Макс Хиты больше 0!");
         }
+        protected string? Actor { get; init; }
         public bool Concentration { get; set; } = false;
-        public Fighter(string name, int initiative, int health, int max_health)
-        {
-            Name = name;
-            Initiative = initiative;
-            Health = health;
-            Max_health = max_health;
-        }
-        public override string ToString() => Name;
+
         public abstract bool Damage(int damage);
-        public override int GetHashCode() => Name.GetHashCode();
+        public override string ToString() => _name;
+        public override int GetHashCode() => _name.GetHashCode();
         public override bool Equals(object? obj)
         {
-            if (obj is Fighter fighter) return Name == fighter.Name;
+            if (obj is Fighter fighter) return _name == fighter._name;
             return false;
         }
-
         public int CompareTo(object? obj)
         {
             if (obj is Fighter fighter)
@@ -52,7 +55,7 @@
                     return -1;
                 else if (Initiative < fighter.Initiative)
                     return 1;
-                else if (actor != fighter.actor && actor == "Hero")
+                else if (Actor != fighter.Actor && Actor == "Hero")
                     return -1;
             return 0;
         }
@@ -61,7 +64,7 @@
     internal class Hero : Fighter
     {
         public Hero(string name, int initiative, int health, int max_health)
-            : base(name, initiative, health, max_health) { actor = "Hero"; }
+            : base(name, initiative, health, max_health) { Actor = "Hero"; }
 
         public override bool Damage(int damage)
         {
@@ -77,7 +80,7 @@
     internal class Villain : Fighter
     {
         public Villain(string name, int initiative, int health, int max_health)
-            : base(name, initiative, health, max_health) { actor = "Villain"; }
+            : base(name, initiative, health, max_health) { Actor = "Villain"; }
 
         public override bool Damage(int damage)
         {
